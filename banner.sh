5e0f695b9b4ae5013df1de9ad3438e16dba5d6fa
Dodo_Surawwoot <dodosurawoot@outlook.com>
2022-07-06 14:27:05 +0700

    Update banner.sh

--- banner.sh
+++ banner.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 if [ "$PS1" ]; then
 #Bash Colour Codes
@@ -14,21 +15,20 @@ fi
 
 echo -e "
 
-╔═══╦╗        ╔╦═══╗   ╔═╗╔╗╔═══╦═══╦═╗╔═╦═══╦══╗╔═╗╔═╗
-║╔ ╗║║        ║║╔═╗║   ║╔╬╝╚╣╔═╗║╔═╗╠╗╚╝╔╣╔═╗║╔╗║╚╗╚╝╔╝
-║║ ╚╣║╔══╦╗╔╦═╝║╚══╦══╦╝╚╬╗╔╩╝╔╝║║ ╚╝╚╗╔╝║╚═╝║╚╝╚╗╚╗╔╝
-║║ ╔╣║║╔╗║║║║╔╗╠══╗║╔╗╠╗╔╝║║╔╗╚╗║║ ╔╗╔╝╚╗║╔══╣╔═╗║╔╝╚╗
-║╚═╝║╚╣╚╝║╚╝║╚╝║╚═╝║╚╝║║║ ║╚╣╚═╝║╚═╝╠╝╔╗╚╣║  ║╚═╝╠╝╔╗╚╗
-╚═══╩═╩══╩══╩══╩═══╩══╝╚╝ ╚═╩═══╩═══╩═╝╚═╩╝  ╚═══╩═╝╚═╝
+  _____  _       ____   _    _  _____    _____   ____   ______  _______    ____    _____ __   __
+ / ____|| |     / __ \ | |  | ||  __ \  / ____| / __ \ |  ____||__   __|  |___ \  / ____|\ \ / /
+| |     | |    | |  | || |  | || |  | || (___  | |  | || |__      | |       __) || |      \ V /
+| |     | |    | |  | || |  | || |  | | \___ \ | |  | ||  __|     | |      |__ < | |       > <
+| |____ | |____| |__| || |__| || |__| | ____) || |__| || |        | |      ___) || |____  / . \
+ \_____||______|\____/  \____/ |_____/ |_____/  \____/ |_|        |_|     |____/  \_____|/_/ \_\
+                                                                                                                                                                                         $
 
-
-
- Linux Version	: ${linux_ver}
- Welcome to  	: `hostname`
- Uptime      	: `uptime | grep -ohe 'up .*' | sed 's/up //g' | awk -F "," '{print $1}'`
- Load        	: `uptime | grep -ohe 'load average[s:][: ].*' | awk '{ print "Last Minute: " $3" Last 5 Minutes: "$4" Last 15 Minutes: "$5 }'`
- Users       	: `uptime | grep -ohe '[0-9.*] user[s,]'`
- IP Address  	: ${green}`ip addr | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | xargs`${txtrst}
+ Linux Version  : ${linux_ver}
+ Welcome to     : "3CX Phone System by Cloudsoft"
+ Uptime         : `uptime | grep -ohe 'up .*' | sed 's/up //g' | awk -F "," '{print $1}'`
+ Load           : `uptime | grep -ohe 'load average[s:][: ].*' | awk '{ print "Last Minute: " $3" Last 5 Minutes: "$4" Last 15 Minutes: "$5 }'`
+ Users          : `uptime | grep -ohe '[0-9.*] user[s,]'`
+ IP Address     : ${green}`ip addr | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | xargs`${txtrst}
  Clock          :`timedatectl | sed -n '/Local time/ s/^[ \t]*Local time:\(.*$\)/\1/p'`
  NTP Sync.      :`timedatectl |awk -F: '/NTP sync/ {print $2}'`
 "
